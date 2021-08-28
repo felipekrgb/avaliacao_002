8f2ef386e2e2e3530f8708770649a45ad5b8e629
felipekrgb <felipekrgb@gmail.com>
2021-08-28 19:35:31 -0300

    Adding ViewModel to Pull Requests

--- app/src/main/java/com/example/avaliacao_002/view_model/PullRequestsViewModel.kt
+++ app/src/main/java/com/example/avaliacao_002/view_model/PullRequestsViewModel.kt
@@ -1,7 +1,31 @@
 package com.example.avaliacao_002.view_model
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.avaliacao_002.model.PullRequest
+import com.example.avaliacao_002.repository.GithubRepository
 
 class PullRequestsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _pullRequests = MutableLiveData<List<PullRequest>>()
+    val pullRequests: LiveData<List<PullRequest>> = _pullRequests
+
+    private val _error = MutableLiveData<String>()
+    val error: LiveData<String> = _error
+
+    private val githubRepository = GithubRepository()
+
+    fun getPullRequestsList(fullname: String) {
+        githubRepository.getPullRequestsList(fullname) { list, error ->
+            list?.apply {
+                _pullRequests.value = this
+            }
+
+            error?.apply {
+                _error.value = this
+            }
+        }
+    }
+
 }
