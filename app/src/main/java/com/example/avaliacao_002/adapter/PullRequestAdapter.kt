708e161b6a9f2b75b840f60aa91c25c2af8dd0c9
felipekrgb <felipekrgb@gmail.com>
2021-08-28 19:24:14 -0300

    Creating adapter for Pull Requests

---
+++ app/src/main/java/com/example/avaliacao_002/adapter/PullRequestAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.avaliacao_002.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.avaliacao_002.R
+import com.example.avaliacao_002.databinding.ItemPullRequestBinding
+import com.example.avaliacao_002.model.PullRequest
+
+class PullRequestAdapter : RecyclerView.Adapter<PullRequestViewHolder>() {
+
+    private var pullRequestsList: MutableList<PullRequest> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PullRequestViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_pull_request, parent, false)
+        return PullRequestViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PullRequestViewHolder, position: Int) {
+        pullRequestsList[position].apply {
+            holder.bind(this)
+        }
+    }
+
+    override fun getItemCount(): Int = pullRequestsList.size
+}
+
+class PullRequestViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    val binding: ItemPullRequestBinding = ItemPullRequestBinding.bind(itemView)
+
+    fun bind(pullRequest: PullRequest) {
+        pullRequest.let {
+            binding.pullRequestTitleTextView.text = it.title
+        }
+
+        pullRequest.user.let {
+            Glide.with(itemView.context).load(it.avatarURL)
+                .into(binding.userAvatarImageView)
+        }
+    }
+
+}
--- app/src/main/java/com/example/avaliacao_002/model/PullRequest.kt
+++ app/src/main/java/com/example/avaliacao_002/model/PullRequest.kt
@@ -1,7 +1,29 @@
 package com.example.avaliacao_002.model
 
+import com.google.gson.annotations.SerializedName
 
 
 data class PullRequest(
     val title: String,
+
+    val user: User,
+
+    val body: String,
+
+    @SerializedName("created_at")
+    val createdAt: String,
+) {
+    fun formattedDate(): String {
+        val dateWithoutHours = createdAt.split("T")[0].split("-")
+
+        return "${dateWithoutHours[2]}/${dateWithoutHours[1]}/${dateWithoutHours[0]}"
+    }
+}
+
+data class User(
+    @SerializedName("login")
+    val username: String,
+
+    @SerializedName("avatar_url")
+    val avatarURL: String
 )
